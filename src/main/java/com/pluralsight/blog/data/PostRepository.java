44468dad72633c3c5ced2fa01ce0ae1270288772
Zaldy Esteban <zaldy1028@outlook.com>
2021-08-14 21:07:28 +0800

    Module 4

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -51,6 +51,11 @@ public class PostRepository {
     }
 
     public Post findById(Long id) {
+        for (Post post : ALL_POSTS) {
+            if (post.getId() == id) {
+                return post;
+            }
+        }
         return null;
     }
 }
